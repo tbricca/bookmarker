3f309876d21183e292ce33006165b263f2e116b9
tbricca <tbricca@gmail.com>
2018-10-02 07:11:50 -0700

    found syntax error, app is working again

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 // Listen for form submit formid "myform"
 document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
-function saveBookmark() {
-  comsole.log("It works");
+/// SAVE BOOKMARK
+function saveBookmark(e) {
+  var siteName = document.getElementById("siteName");
+  console.log(siteName);
+  // prevent form from submitting
+  e.preventDefault();
 }

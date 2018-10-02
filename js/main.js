92bf7ccbc17173d467a7cec52bc0fefac1dc4c7a
tbricca <tbricca@gmail.com>
2018-10-02 07:29:53 -0700

    got it to create an object, prepping it for sending it to local storage

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
 /// SAVE BOOKMARK
 function saveBookmark(e) {
-  var siteName = document.getElementById("siteName");
-  console.log(siteName);
+  var siteName = document.getElementById("siteName").value;
+  var siteUrl = document.getElementById("siteUrl").value;
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  };
+
+  console.log(bookmark);
   // prevent form from submitting
   e.preventDefault();
 }
